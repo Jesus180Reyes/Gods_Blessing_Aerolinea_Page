24dd3b88606cca137f674916113c3308153ed436
Jesus180Reyes <luisdejesus200122@gmail.com>
2021-11-29 20:04:42 -0600

    Pagina Gods Blessing Terminada

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = ()=> {
+    navbar.classList.toggle('active');
+}
+
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () => {
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+});
+
+
+$(function () {
+    $('#myDiv').floatingWhatsApp({
+        phone: '+504 8851-3318',
+        
+        showPopup: true,
+        position: 'right',
+        popupMessage: 'Hola, En que te puedo Ayudar',
+      message: 'Hola queria cotizar un viaje!',
+      headerTilte: 'Bienvenido',
+      autoOpenTimeout: 7000,
+      size: '60px',
+      showOnIE: false,
+    });
+  });
