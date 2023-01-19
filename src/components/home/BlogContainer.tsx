401bc73e45559ef44b35fe56f9abd8696e0db691
Jesus Reyes <luisdejesus200122@gmail.com>
2023-01-19 15:20:35 -0600

    Feat: Blog Section Added

---
+++ src/components/home/BlogContainer.tsx
@@ -0,0 +1,29 @@
+
+export const BlogContainer = () => {
+  return (
+    <>
+    <div className="blog-container">
+      <h1>Blog</h1>
+      <div data-aos="fade-up" className="row-blogs">
+        <div className="card-blog-content">
+          <img src="https://firebasestorage.googleapis.com/v0/b/fir-f16bc.appspot.com/o/blog1.jpg?alt=media&token=a3982572-d30c-4ea6-84f4-240e958b696b"  />
+          <p>Dic 20, 2022</p>
+          <h3>2022 Travel Trends - what you need to know</h3>
+          <p>Qui velit esse officia consequat ut labore cillum velit.</p>
+
+        </div>
+        <div className="card-blog-content">
+          <img src="https://firebasestorage.googleapis.com/v0/b/fir-f16bc.appspot.com/o/blog2.jpg?alt=media&token=34ddd6ab-d2f2-435d-866e-26aa3c58d40c"  />
+          <p>Nov 14, 2022</p>
+
+          <h3>China's airlines launch new routes for 2023</h3>
+          <p>Elit cupidatat non qui cupidatat in est et eiusmod est. Proident Lorem mollit occaecat anim Lorem. Est sunt anim incididunt ea qui laborum fugiat aute culpa. Id ut commodo culpa in dolor. Eiusmod laboris consectetur proident enim ea. Laboris velit duis velit incididunt exercitation.
+Laboris minim aute enim aliqua veniam mollit adipisicing quis quis qui quis ipsum. Consequat enim in voluptate excepteur fugiat nisi. Ut minim sint quis fugiat ea mollit aute consequat sunt dolore. Laborum est consequat aliquip eu pariatur aute. Ut aliquip eiusmod laboris anim commodo minim aliquip ea labore ut irure sunt. Occaecat dolore do dolor aute est ullamco dolor. Culpa culpa ad eiusmod cupidatat ex reprehenderit aliqua velit dolore deserunt qui veniam veniam.</p>
+
+        </div>
+      </div>
+      <button className="btn-form">Ver mas</button>
+    </div>
+    </>
+  )
+}
--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,3 +1,4 @@
+export { BlogContainer } from './BlogContainer';
 export { WhyFly } from './WhyFly';
 export { ArticleContent } from './ArticleContent';
 export { RowArticleFeat } from './RowArticleFeat';
