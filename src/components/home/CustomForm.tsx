d3d7f484e13f601a33a35b934ec70d80bf059e0b
Jesus Reyes <luisdejesus200122@gmail.com>
2023-02-22 12:12:11 -0600

    Breaking Change: Bug Fixe On Home_Page Form

--- src/components/home/CustomForm.tsx
+++ src/components/home/CustomForm.tsx
@@ -1,25 +1,29 @@
 import { customAlert } from '../../helpers/custom_alert';
 import emailjs from '@emailjs/browser';
 import { useFormHome } from '../../hooks/';
+import { useState } from 'react';
+import { LoadingPage } from '../../shared/LoadingPage';
 export const CustomForm = () => {
   const {phoneNumber,destiny,dateAtComeback,dateAtGo,resetInputsForm, onInputDateAtComebackFormChange,onInputDateAtGoFormChange,onInputDestinyFormChange,onInputNumberFormChange} = useFormHome();
-
-  const onSubmit = (e:any)=> {
+  const [isLoading, setIsLoading] = useState<boolean>();
+  const onSubmit = async(e:any)=> {
     e.preventDefault();
     if(phoneNumber.length <=1 )return customAlert("El numero de telefono es obligatorio","Rellena todos los campos requeridos", "error");
     if(phoneNumber.length < 8 )return customAlert("Numero de telefono no valido","Rellena todos los campos requeridos", "error");
     if(destiny.length <=1 )return customAlert("El destino es obligatorio","Rellena todos los campos requeridos", "error");
     if(dateAtGo.length <=1 )return customAlert("La fecha de ida es obligatorio","Rellena todos los campos requeridos", "error");
     if(dateAtComeback.length <=1 )return customAlert("La fecha de Vuelta es obligatorio","Rellena todos los campos requeridos", "error");
+    setIsLoading(true);
     const message = {
       phoneNumber,
       destiny,
       dateAtGo,
       dateAtComeback,
     }
-    emailjs.send(import.meta.env.VITE_SERVICE_ID,import.meta.env.VITE_TEMPLATE_ID_HOME,message, import.meta.env.VITE_PUBLIC_KEY);
+    await emailjs.send(import.meta.env.VITE_SERVICE_ID,import.meta.env.VITE_TEMPLATE_ID_HOME,message, import.meta.env.VITE_PUBLIC_KEY);
     resetInputsForm();
     customAlert("Cotizacion enviada!!","La Cotizacion fue enviada exitosamente, muy pronto nos comunicaremos contigo","success");
+    setIsLoading(false);
   }
   return (
     <>
@@ -41,7 +45,7 @@ export const CustomForm = () => {
         <p>Vuelta:</p>
         <input type="date"  value={dateAtComeback} onChange={onInputDateAtComebackFormChange}/>
       </div>
-      <button className="btn-form" onClick={onSubmit} >Cotizar Vuelo</button>
+      <button disabled={isLoading} className="btn-form" onClick={onSubmit} >{!isLoading ? "Cotizar Vuelo" : "Enviando datos..."}</button>
     </div>
     </form>    
     </>
